5aa7097b7591875925b10f12bbf206e0f7303c37
rparadaumanzor <82298391+rparadaumanzor@users.noreply.github.com>
2023-05-18 15:24:31 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -18,3 +18,38 @@ class agent:
             layers.Dense(6)
         ])
         
+    def get_model_name(self):
+        return 'taxi.h5'
+    
+    def save_model(self):
+        self.model.save(self.get_model_name())
+        
+    def next_action(self, state):
+        
+        if random.random() <= self.epsilon:
+            # If <= epsilon, perform a random action 
+            # i.e. jump or don't jump (with equal probability)
+            return random.randint(0, 5)
+        else:
+            # If > epsilon, perform a greedy action (use best known action)
+            return self.Qfunction(state)
+            
+    def train(self, current_state, next_state, reward):
+        
+        Q_current_state = self.Qfunction(current_state)
+        Q_next_state = self.Qfunction(next_state)
+        
+        Q_current_state = ((1-self.alpha) * Q_current_state) + (self.alpha * (reward + self.gamma * Q_next_state))
+        
+        # Pass through neural network
+    
+    def Qfunction(self, state):
+        
+        # Want to compute "something".predict(current_state)
+        # return something (depending on what that something is may need to return some index... 
+        # not entirely sure what is gonna get returned by the neural network)
+        pass
+    
+    def update(self, current_state, next_state, reward):
+        self.train(current_state, next_state, reward)
+        self.epsilon *= self.epsilon_decay
