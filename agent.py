001ead422b1901c36b738a9dfabb47c697ee4456
rparadaumanzor <82298391+rparadaumanzor@users.noreply.github.com>
2023-05-23 22:54:53 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,31 +5,31 @@ from inputs import path
 class agent:
 	
 	def __init__(self,  gamma = 0.9, epsilon = 0.9, epsilon_decay = 0.995, alpha = 0.01, state_size=500, action_size=6):
-        
-        self.state_size = state_size
-        self.action_size = action_size
-        
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_decay = epsilon_decay
-        
-        self.alpha = alpha
-        
-        self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(128, activation='relu', input_shape=(self.state_size,)),
-            
-            tf.keras.layers.Dense(64, activation = 'relu'),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(32, activation = 'linear'), # Potentially change to ReLu
-            
-            tf.keras.layers.Dense(self.action_size, activation = 'linear')
-            ])
-        
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.alpha)
-        self.loss_fn = tf.keras.losses.MeanSquaredError()
-        self.model.compile(optimizer = self.optimizer,
-                           loss = self.loss_fn,
-                           metrics = ['accuracy'])
+		
+		self.state_size = state_size
+		self.action_size = action_size
+
+		self.gamma = gamma
+		self.epsilon = epsilon
+		self.epsilon_decay = epsilon_decay
+
+		self.alpha = alpha
+
+		self.model = tf.keras.Sequential([
+		    tf.keras.layers.Dense(128, activation='relu', input_shape=(self.state_size,)),
+
+		    tf.keras.layers.Dense(64, activation = 'relu'),
+		    tf.keras.layers.Dropout(0.2),
+		    tf.keras.layers.Dense(32, activation = 'linear'), # Potentially change to ReLu
+
+		    tf.keras.layers.Dense(self.action_size, activation = 'linear')
+		    ])
+
+		self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.alpha)
+		self.loss_fn = tf.keras.losses.MeanSquaredError()
+		self.model.compile(optimizer = self.optimizer,
+				   loss = self.loss_fn,
+				   metrics = ['accuracy'])
 
 
         
@@ -44,12 +44,14 @@ class agent:
 
 	     
 	def next_action(self, state):
-        
-		if np.random.rand() <= epsilon:
-			action = env.action_space.sample()
+		
+		if np.random.rand() <= self.epsilon:
+		    action = np.random.randint(self.action_size)
 		else:
-			q_values = model.predict(np.expand_dims(state, axis=0))
-			action = np.argmax(q_values)
+		    q_values = self.model.predict(np.expand_dims(state, axis = 0))
+		    action = np.argmax(q_values)
+
+		return action
             
 	def train(self, current_state, next_state, reward, done):
 		target = reward
@@ -59,13 +61,18 @@ class agent:
         
         
     
-	def Qfunction(self, state):
-		q_values = model.predict(np.expand_dims(state, axis=0))
-		next_q_values = model.predict(np.expand_dims(next_state, axis=0))
-		q_values[0][action] = reward + gamma * np.max(next_q_values)
-    
-	'''
-	def update(self, current_state, next_state, reward):
-		self.train(current_state, next_state, reward)
-		self.epsilon *= self.epsilon_decay
-	'''
+	def update_model(self, state, action, reward, next_state, done):
+        
+		q_values = self.model.predict(np.expand_dims(state, axis = 0))
+		next_q_values = self.model.predict(np.expand_dims(next_state, axis = 0))
+		q_values[0][action] = reward + self.gamma * np.max(next_q_values)
+
+		with tf.GradientTape() as tape:
+		    q_values_pred = self.model(np.expand_dims(state, axis = 0))
+		    loss_value = self.loss_fn(q_values_pred, q_values)
+
+		grads = tape.gradient(loss_value, self.model.trainable_variables)
+		self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+		if done:
+		    self.epsilon *= self.epsilon_decay
