a2e7119e884c52af1a6e6bd10048aec4c390d078
rparadaumanzor <82298391+rparadaumanzor@users.noreply.github.com>
2023-05-23 22:53:24 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,33 +1,35 @@
 import numpy as np
 import tensorflow as tf
-import gym
-import random 
 from inputs import path
 
 class agent:
-	def __init__(self,  gamma = 0.9, epsilon = 0.9, epsilon_decay = 0.995, alpha = 0.1, state_size=500, action_size=6):
-     
-
-		self.state_size = state_size
-		self.action_size = action_size
-       
-		self.gamma = gamma
-		self.epsilon = epsilon
-		self.epsilon_decay = epsilon_decay
+	
+	def __init__(self,  gamma = 0.9, epsilon = 0.9, epsilon_decay = 0.995, alpha = 0.01, state_size=500, action_size=6):
         
-		self.alpha = alpha
+        self.state_size = state_size
+        self.action_size = action_size
         
-		self.model = tf.keras.Sequential([
-			tf.keras.layers.Dense(128, activation='relu', input_shape=(state_size,)),
-			tf.keras.layers.Dense(64, activation = 'relu'),
-			tf.keras.layers.Dropout(0.2),
-			tf.keras.layers.Dense(32, activation = 'linear'),
-			tf.keras.layers.Dense(self.action_size, activation='linear')
-			])
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
         
-		self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-		self.loss_fn = tf.keras.losses.MeanSquaredError()
-		self.model.compile(optimizer=self.optimizer, loss = self.loss_fn)
+        self.alpha = alpha
+        
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation='relu', input_shape=(self.state_size,)),
+            
+            tf.keras.layers.Dense(64, activation = 'relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(32, activation = 'linear'), # Potentially change to ReLu
+            
+            tf.keras.layers.Dense(self.action_size, activation = 'linear')
+            ])
+        
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.alpha)
+        self.loss_fn = tf.keras.losses.MeanSquaredError()
+        self.model.compile(optimizer = self.optimizer,
+                           loss = self.loss_fn,
+                           metrics = ['accuracy'])
 
 
         
