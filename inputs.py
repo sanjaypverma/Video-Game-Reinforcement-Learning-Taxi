e073ca544dd22c5f52b545a5765c83b014f5263c
Iliana Marrujo <ilianamarrujo@IlianaMacBook.local>
2023-05-25 19:36:06 -0700

    added continue training

--- inputs.py
+++ inputs.py
@@ -1,3 +1,6 @@
 ##inputs 
 
+
 path = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/save_model'
+
+complete_model = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/completemodel'
