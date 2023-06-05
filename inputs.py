a2a2cd799d18d0ab49882da8a4893153b71eef1e
ilimarrujo <107446045+ilimarrujo@users.noreply.github.com>
2023-06-04 17:44:02 -0700

    Create inputs.py

---
+++ inputs.py
@@ -0,0 +1,7 @@
+#Path files 
+
+complete_model = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/save_complete_model'
+progress_model = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/save_progress'
+
+graph_data = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/output_data/graph_data.csv'
+save_animation = '/Users/ilianamarrujo/computing16B/project/PIC16BProject/output_data/trained_agent_animation.mp4'
