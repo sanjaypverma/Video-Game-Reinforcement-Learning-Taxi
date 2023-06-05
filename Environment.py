f4dcb95fa34b0425781eef6081455e2ef7fe5ad3
ilimarrujo <107446045+ilimarrujo@users.noreply.github.com>
2023-06-04 17:43:36 -0700

    put Environment.py in own file

---
+++ Environment.py
@@ -0,0 +1,23 @@
+#Environment Class
+
+class Environment:
+    def __init__(self, env_name):
+        
+        self.env = gym.make(env_name)
+        self.state_size = self.env.observation_space.n
+        self.action_size = self.env.action_space.n
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        return self.env.step(action)
+
+    def render(self):
+        self.env.render()
+
+    def close(self):
+        self.env.close()
+
+    def vectorize_state(self, state):
+        return tf.keras.utils.to_categorical(state, num_classes=self.state_size).reshape(1, -1)
