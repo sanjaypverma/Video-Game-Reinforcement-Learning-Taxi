31830d8742ce6210c16d77e67d077a5a7af34392
ilimarrujo <107446045+ilimarrujo@users.noreply.github.com>
2023-06-11 22:02:09 -0700

    Update Model1.py

--- Model1.py
+++ Model1.py
@@ -243,27 +243,6 @@ def main():
 
     time.sleep(10)
 
-    # Watch trained agent
-#     state = env.reset()
-#     state = env.vectorize_state(state)
-#     done = False
-#     rewards = 0
-
-#     for s in range(max_steps):
-#         print("TRAINED AGENT")
-#         print("Step {}".format(s + 1))
-
-#         action = agent.act(state)
-#         next_state, reward, done, info = env.step(action)
-#         rewards += reward
-#         env.render()
-#         print(f"Score: {rewards}")
-#         state = env.vectorize_state(next_state)
-
-#         if done:
-#             break
-
-
 
 #####################
     state = env.reset()
@@ -286,54 +265,6 @@ def main():
             break
 
 
-#     def visualization(ss):
-#         #clear previous screenshot 
-#         plt.clf()
-#         env.render()
-
-#         state = positionPlay[ss]
-#         agent_position = np.unravel_index(state.argmax(), state.shape)
-#         plt.scatter(agent_position[1], agent_position[0], color='red', s=100)
-
-#         plt.title(f"Step: {ss + 1}")
-
-#     fig = plt.figure()
-#     game_animation = animation.FuncAnimation(fig, visualization, frame_num=len(positionPlay), interval=500, repeat=False)
-#     game_animation.save(save_animation, writer='ffmpeg')
-    
-#     plt.show()
-
-
-
-#     state = env.reset()
-#     done = False
-#     rewards = 0
-
-#     # this loop is for the animation so you can visually see
-#     # how the agent is performing.
-#     for s in range(max_steps):
-
-#         print(f"TRAINED AGENT")
-#         print("Step {}".format(s+1))
-
-#         # exploit a known action, we'll only used the
-#         # exploitation since the agent is aleady trained
-#         action = agent.act(state)
-#         # take the action in the environment
-#         new_state, reward, done, info = env.step(action)
-#         # update reward
-#         rewards += reward
-#         # update the screenshot of the environment
-#         env.render()
-
-#         print(f"score: {rewards}")
-#         state = new_state
-
-#         if done == True:
-#             break
-            
-#         if done:
-#             break
     env.close()
 
 
